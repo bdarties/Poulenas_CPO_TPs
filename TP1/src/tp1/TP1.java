9f2f44c4ef99f10b5e191e9cfd56efca2fd384e0
Poulenas <Poulenas@LAPTOP-LHDB2PDG>
2021-10-04 11:09:40 +0200

    Depôt exercice 1 TP1

--- TP1/src/tp1/TP1.java
+++ TP1/src/tp1/TP1.java
@@ -3,18 +3,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package tp1;
+import java.util.Scanner;
 
-/**
+ /**
  *
  * @author Poulenas
+ *Poulenas Sandra
+ * TDA
+ * TP1 saisie et manipulation de nombre
+ * 04/10/2021
  */
+ 
 public class TP1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc; 
+        sc = new Scanner(System.in);
+        int nombre1;
+        int nombre2;
+        System.out.println("Saissir un premier nombre");
+        nombre1=sc.nextInt();
+        System.out.println("Saisir un second nombre");
+        nombre2=sc.nextInt();
+        int somme;
+        somme = nombre1+nombre2;
+        System.out.println("La somme est :"+somme);
+        int difference;
+        difference = nombre1-nombre2;
+        System.out.println("La difference est :"+difference);
+        int produit;
+        produit=nombre1*nombre2;
+        System.out.println("Le produit est :"+produit);
+        int quotien;
+        quotien = nombre1/nombre2;
+        System.out.println("Le quotien entier vaut :"+quotien);
+        int reste;
+        reste =nombre1%nombre2;
+        System.out.println("Le reste de la division est"+reste);
+             
+                
     }
-    
 }
