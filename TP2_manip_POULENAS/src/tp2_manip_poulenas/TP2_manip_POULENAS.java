c93ebe1976796318d10e397e0d0cdfaad85f9939
Poulenas <Poulenas@LAPTOP-LHDB2PDG>
2021-10-18 22:55:12 +0200

    Depôt exercice 3 TP2 Pas encore fini

---
+++ TP2_manip_POULENAS/src/tp2_manip_poulenas/TP2_manip_POULENAS.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_manip_poulenas;
+
+/**
+ *
+ * @author Poulenas
+ * Poulenas Sandra
+ * TDA
+ * Exercie 3 manipulation
+ * 18.10.2021
+ */
+public class TP2_manip_POULENAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Tartiflette assiette1 = new Tartiflette(500) ;
+        Tartiflette assiette2 = new Tartiflette(600) ;
+        Tartiflette assiette3 = assiette2 ; 
+        System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories) ;
+        System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories) ;
+
+    }
+    
+}
